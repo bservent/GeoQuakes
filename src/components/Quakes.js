52c68f646a59a8bd7b35f700a49b9970b542b0f4
Brittany Servent <brittybear@Brittanys-MBP.domain>
2020-11-19 16:27:28 -0800

    color swap

--- src/components/Quakes.js
+++ src/components/Quakes.js
@@ -3,18 +3,18 @@ import Quake from './Quake';
 class Quakes extends Component {
 
   numToColor(num){
-    return num >= 6 ? 'purple':
-      num >= 5 ? 'red':
+    return num >= 6 ? 'red':
+      num >= 5 ? 'violet':
         'orange'
   }
 
   render(){
-    let quakesList = this.props.quakes.map( (elem, idx) =>{
+    let quakesList = this.props.quakes.map( (elem, index) =>{
 
       return <Quake  
       shake={elem.properties.mag >= 6} 
       color={this.numToColor(elem.properties.mag)} 
-      key={idx} mag={elem.properties.mag} 
+      key={index} mag={elem.properties.mag} 
       title={elem.properties.title}  
       /> 
     })
